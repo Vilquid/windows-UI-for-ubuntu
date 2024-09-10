be79b2a055ea97e3c90acd115550935a6fb9e3f4
Vilquid <60876471+Vilquid@users.noreply.github.com>
2024-09-10 19:24:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,4 +60,22 @@ fi
 gsettings set org.gnome.desktop.interface font-name 'Segoe UI Light'
 
 # install the extension Dash to Panel (1160 dash-to-panel)
-echo "Installation of Dash to Panel"
+sudo apt install gnome-tweaks -y
+echo "Please, install Dash to Panel extension"
+echo "Open Gnome Tweaks, go to Extensions, click on the gear icon of Dash to Panel and set Size to desktop icons to Tiny"
+echo "and Left box/Taskbar to Monitor Center"
+echo "Move Date at the almost lowest position"
+echo "Go to style tab"
+echo "Set App icon Margin to 0 px and App icon padding to 6 px"
+echo "Set Running indicator style to Dashes and Running indicator style to Dashes"
+echo "Go to behaviour tab"
+echo "Uncheck click empty space to close overview"
+echo "Check Disable show overview on startup"
+echo "Go to fine tune tab"
+echo "Set :"
+echo "  -  Tray font size, Left box front size to 16"
+echo "  -  Tray item padding, status icon padding and leftbox padding to 4"
+echo ""
+echo "Please, install ArcMenu extension"
+echo "open ArcMenu settings"
+https://www.youtube.com/watch?v=x7LEHV4LRpU
